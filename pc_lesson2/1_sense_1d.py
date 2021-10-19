18ef639572a8030d9824d9b23e0598bf3e2c7613
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2021-10-19 21:11:29 +0100

    lesson2 ex1 resolution

--- pc_lesson2/1_sense_1d.py
+++ pc_lesson2/1_sense_1d.py
@@ -1,7 +1,7 @@
 colors = ['red', 'green', 'green', 'red' , 'red']
 
 measurements = ['green']                                       # test 1
-#measurements = ['green', 'green', 'green' ,'green', 'green']   # test 2
+measurements = ['green', 'green', 'green' ,'green', 'green']   # test 2
 
 sensor_right = {}
 sensor_right['green'] = 0.6
@@ -12,19 +12,29 @@ def sense(p, Z):
     """Update belief array p according to new measurement Z"""
     # TODO: insert your code here
 
+    for i in range(len(p)):
+        if Z == colors[i]:
+            p[i] = p[i] * sensor_right['green']
+        else:
+            p[i] = p[i] * (1.0 - sensor_right['red'])        
+
+    norm = [float(i)/sum(p) for i in p]
+    p = norm
+
     return p
 
 #main
-p = []
+if __name__ == '__main__':
+    p = []
 
-width  = len(colors)
-n = width
+    width  = len(colors)
+    n = width
 
-for c in range(width):
-    p.append(1./n)
+    for c in range(width):
+        p.append(1./n)
 
-for s in range(len(measurements)):
-    p = sense(p,measurements[s])
+    for s in range(len(measurements)):
+        p = sense(p,measurements[s])
 
-print p
+    print(p)
 
