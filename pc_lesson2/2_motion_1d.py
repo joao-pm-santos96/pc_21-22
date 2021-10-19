749ca84b2917bf1ce116ce789f51d4b1fe7e2434
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2021-10-19 21:49:06 +0100

    lesson2 Ex2 resolution

--- pc_lesson2/2_motion_1d.py
+++ pc_lesson2/2_motion_1d.py
@@ -1,24 +1,51 @@
 colors = ['red', 'green', 'green', 'red' , 'red']
 
 motions = [[1]]                      # test 1
-#motions = [[1],[0],[-1],[1],[0]]     # test 2
+motions = [[1],[0],[-1],[1],[0]]     # test 2
+# test 2 solution: .148 .42 .352 .064 .016
 
 p_move = 0.8
+
+def compute_right(p):
+    p_aux = []
+    for i in range(len(p)):
+        if i == 0:
+            p_aux.append(p_move * p[len(p) - 1] + (1-p_move) * p[i])
+        else:
+            p_aux.append(p_move * p[i-1] + (1-p_move) * p[i])
+
+    return p_aux
+
+def compute_left(p):
+    p_aux = []
+    for i in range(len(p)):
+        if i == (len(p) - 1):
+            p_aux.append(p_move * p[0] + (1 - p_move) * p[i])
+        else:
+            p_aux.append(p_move * p[i + 1] + (1 - p_move) * p[i])
+
+    return p_aux
 
 def move(p, U):
     """Update p after movement U"""
     #TODO: insert your code here
 
+    if U[0] == 1:
+        p = compute_right(p)
+    elif U[0] == -1:
+        p = compute_left(p)
+
     return p
 
 #main
-p = [0.5, 0.5, 0, 0, 0]
+if __name__ == '__main__':
+    p = [0.5, 0.5, 0, 0, 0]
 
-width  = len(colors)
-n = width
+    width  = len(colors)
+    n = width
 
-for s in range(len(motions)):
-    p = move(p,motions[s])
+    for s in range(len(motions)):
+        p = move(p,motions[s])
 
-print p
+    print(p)
 
